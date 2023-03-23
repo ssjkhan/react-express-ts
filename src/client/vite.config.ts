3ac334ea8c5c310b4f7f983f2e57d62726fa7f57
sk <jsckhan@outlook.com>
2023-03-23 19:28:18 -0400

    feat: client production build files
    Update client config and base html for production config and proxy to
    back end server.

--- src/client/vite.config.ts
+++ src/client/vite.config.ts
@@ -9,17 +9,34 @@ import react from "@vitejs/plugin-react-swc";
 // const inputPath = (mode === "development") ? "./src/main.tsx" : false;
 console.log(process.env.NODE_ENV);
 // https://vitejs.dev/config/
+
+// dev config
+// export default defineConfig({
+//   plugins: [react()],
+//   build: {
+//     outDir: "./dist",
+//     manifest: true,
+//     rollupOptions: {
+//       input: "./src/main.tsx",
+//     },
+//   },
+//   server: {
+//     port: 5000,
+//     origin: "http://127.0.0.1:3000",
+//   },
+// });
+
+// prod config
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: "./dist",
+    outDir: "../../build/client/dist",
     manifest: true,
     rollupOptions: {
-      input: "./src/main.tsx",
+      input: "index.dist.html",
     },
   },
   server: {
-    port: 5000,
     origin: "http://127.0.0.1:3000",
   },
 });
